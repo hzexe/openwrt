0875de23dd4d6ce98d61a34ffecc26bcd7ed4737
hzexe <hzexe1@v3.hzexe.com>
2019-03-08 21:10:27 +0800

    add B70

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+apt update -y
+apt install -y  gcc g++ binutils patch bzip2 flex bison make autoconf gettext texinfo unzip sharutils subversion libncurses5-dev ncurses-term zlib1g-dev gawk asciidoc libz-dev git
+cd openwrt 
+./scripts/feeds update -a
+./scripts/feeds install -a
+make -j2 V=s
+
